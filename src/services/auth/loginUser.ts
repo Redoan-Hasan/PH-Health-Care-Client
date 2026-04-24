332247be88cdabe566d8471b5f241f2623634945
Redoan Hasan <redoanhasan57@gmail.com>
2026-04-24 17:46:58 +0600

    access token and refresh token set

--- src/services/auth/loginUser.ts
+++ src/services/auth/loginUser.ts
@@ -1,48 +1,92 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
-
+import { parse } from "cookie";
+import { cookies } from "next/headers";
 import z from "zod";
 const loginUserZodSchema = z.object({
-  email:z.email({
-    error: "Please enter a valid email address"
+  email: z.email({
+    error: "Please enter a valid email address",
   }),
-  password:z.string().min(6,{ error: "Password must be at least 6 characters long"}),
-})
+  password: z
+    .string()
+    .min(6, { error: "Password must be at least 6 characters long" }),
+});
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export  const loginUser = async (currentState:any, formData:any) => {
+export const loginUser = async (currentState: any, formData: any) => {
   try {
+    let accessTokenObject: null | any = null;
+    let refreshTokenObject: null | any = null;
     const loginData = {
       email: formData.get("email"),
       password: formData.get("password"),
     };
     const validatedData = loginUserZodSchema.safeParse(loginData);
-    if(!validatedData.success) {
+    if (!validatedData.success) {
       return {
         success: false,
-        errors: validatedData.error.issues.map((issue) =>{
+        errors: validatedData.error.issues.map((issue) => {
           return {
             field: issue.path[0],
             message: issue.message,
-          }
-        })
-      }
+          };
+        }),
+      };
     }
-    console.log(validatedData,"validatedData");
+    console.log(validatedData, "validatedData");
     const response = await fetch("http://localhost:5000/api/v1/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(loginData),
-    })
+    });
     const result = await response.json();
     console.log("result", result);
     console.log("response", response);
-    const setCookiesHeader = response.headers.getSetCookie();
-    console.log("setCookiesHeader", setCookiesHeader);
+    const setCookieHeader = response.headers.getSetCookie();
+    console.log("setCookiesHeader", setCookieHeader);
+    if (setCookieHeader && setCookieHeader.length > 0) {
+      setCookieHeader.forEach((cookie) => {
+        console.log(cookie, "cookie");
+        const parsedCookie = parse(cookie);
+        console.log("parsedCookie", parsedCookie);
+        if (parsedCookie.accessToken) {
+          accessTokenObject = parsedCookie;
+        }
+        if (parsedCookie.refreshToken) {
+          refreshTokenObject = parsedCookie;
+        }
+      });
+    } else {
+      throw new Error("Failed to retrieve authentication cookies.");
+    }
+    if (!accessTokenObject) {
+      throw new Error("Access token not found in cookies.");
+    }
+    if (!refreshTokenObject) {
+      throw new Error("Refresh token not found in cookies.");
+    }
+    console.log("accessToken", accessTokenObject, "refreshToken", refreshTokenObject);
+    const storeCookie = await cookies();
+    storeCookie.set("accessToken", accessTokenObject.accessToken,{
+      httpOnly: true,
+      secure: true,
+      sameSite: accessTokenObject.sameSite,
+      path: accessTokenObject.path || "/",
+      maxAge: parseInt(accessTokenObject.maxAge)
+    });
+    storeCookie.set("refreshToken", refreshTokenObject.refreshToken,{
+      httpOnly: true,
+      secure: true,
+      sameSite: refreshTokenObject.sameSite,
+      path: refreshTokenObject.path || "/",
+      maxAge: parseInt(refreshTokenObject.maxAge)
+    });
     return result;
   } catch (error) {
     console.log(error);
-    return {error: "An error occurred while logging in. Please try again later.",}
+    return {
+      error: "An error occurred while logging in. Please try again later.",
+    };
   }
-}
+};
