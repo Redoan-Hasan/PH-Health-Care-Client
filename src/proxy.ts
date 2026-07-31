b26c946c37817910da0ba55c4d68f9093dff34ba
Redoan-Hasan <redoanhasan57@gmail.com>
2026-08-01 04:07:04 +0600

    initial proxy file setup done

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,25 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL('/dashboard', request.url))
+  // return NextResponse.redirect(new URL('/register', request.url))
+  console.log("pathname", request.nextUrl.pathname);
+  return NextResponse.next();
 }
- 
+
 // Alternatively, you can use a default export:
 // export default function proxy(request: NextRequest) { ... }
- 
+
 export const config = {
-  matcher: '/',
-}
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.well-known).*)",
+  ],
+};
