2e7b1c3d791893693e82c7dc5d5b4eba0a09bdf2
Redoan-Hasan <redoanhasan57@gmail.com>
2026-08-30 04:52:27 +0600

    working on dashboard ui

---
+++ src/components/ui/modules/dashboard/DashboardNavbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DashboardNavbarContent from './DashboardNavbarContent';
+
+const DashboardNavbar = () => {
+    return <DashboardNavbarContent />;
+};
+
+export default DashboardNavbar;
---
+++ src/components/ui/modules/dashboard/DashboardNavbarContent.tsx
@@ -0,0 +1,10 @@
+"use client"
+const DashboardNavbarContent = () => {
+    return (
+        <div>
+            Dashboard Navbar Content
+        </div>
+    );
+};
+
+export default DashboardNavbarContent;
